5403276891e528948b897a222adca1dbaa09ef15
Frank Denis <github@pureftpd.org>
2019-01-08 15:01:58 +0100

    Unroll
    (we may want to use memory.Copy() again later)

--- assembly/index.ts
+++ assembly/index.ts
@@ -464,9 +464,22 @@ let I = fe25519([
 ]);
 
 @inline function fe25519Copy(r: Int64Array, a: Int64Array): void {
-    for (let i = 0; i < 16; ++i) {
-        r[i] = a[i];
-    }
+    r[0] = unchecked(a[0]);
+    r[1] = unchecked(a[1]);
+    r[2] = unchecked(a[2]);
+    r[3] = unchecked(a[3]);
+    r[4] = unchecked(a[4]);
+    r[5] = unchecked(a[5]);
+    r[6] = unchecked(a[6]);
+    r[7] = unchecked(a[7]);
+    r[8] = unchecked(a[8]);
+    r[9] = unchecked(a[9]);
+    r[10] = unchecked(a[10]);
+    r[11] = unchecked(a[11]);
+    r[12] = unchecked(a[12]);
+    r[13] = unchecked(a[13]);
+    r[14] = unchecked(a[14]);
+    r[15] = unchecked(a[15]);
 }
 
 @inline function fe25519Cmov(p: Int64Array, q: Int64Array, b: i64): void {

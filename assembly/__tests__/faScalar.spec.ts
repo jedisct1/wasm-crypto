18aaa6376d13538f5181d7b27a67bd6a1b81f1a6
Frank Denis <github@pureftpd.org>
2019-03-11 18:42:32 +0100

    More tests

--- assembly/__tests__/faScalar.spec.ts
+++ assembly/__tests__/faScalar.spec.ts
@@ -15,7 +15,32 @@ describe("field arithmetic using scalars", (): void => {
     _10[0] = 10;
     let y10 = faScalarMult(y, _10);
     let x_plus_y10 = faScalarAdd(x, y10);
-
     expect<bool>(equals(x_plus_y_times_10, x_plus_y10)).toBeTruthy();
+
+    let one = new Uint8Array(32);
+    one[0] = 1;
+    let minus_one = faScalarNegate(one);
+    expect<u8>(minus_one[0]).toStrictEqual(236);
+    expect<u8>(minus_one[31]).toStrictEqual(16);
+
+    let zero = new Uint8Array(32);
+    let minus_one2 = faScalarSub(zero, one);
+    expect<bool>(equals(minus_one, minus_one2)).toBeTruthy();
+
+    let x2 = faScalarSub(x_plus_y10, y10);
+    x = faScalarReduce(x);
+    expect<bool>(equals(x, x2)).toBeTruthy();
+
+    let x_inv = faScalarInverse(x);
+    let x_x_inv = faScalarMult(x, x_inv);
+    expect<bool>(equals(x_x_inv, one)).toBeTruthy();
+
+    let x_comp = faScalarComplement(x);
+    let x_plus_x_comp = faScalarAdd(x, x_comp);
+    expect<bool>(equals(x_plus_x_comp, one)).toBeTruthy();
+
+    let x_neg = faScalarNegate(x);
+    let x_plus_x_neg = faScalarAdd(x, x_neg);
+    expect<bool>(equals(x_plus_x_neg, zero)).toBeTruthy();
   });
 });

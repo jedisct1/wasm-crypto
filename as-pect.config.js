2138d9953cb17a6baf287ffcfc1cb0e4ca872891
Frank Denis <github@pureftpd.org>
2025-12-27 11:05:05 +0100

    Update deps

--- as-pect.config.js
+++ as-pect.config.js
@@ -1,31 +1,14 @@
-module.exports = {
-  /**
-   * A set of globs passed to the glob package that qualify typescript files for testing.
-   */
-  include: ["assembly/__tests__/**/*.spec.ts"],
-  /**
-   * A set of globs passed to the glob package that quality files to be added to each test.
-   */
-  add: ["assembly/__tests__/**/*.include.ts"],
-  /**
-   * All the compiler flags needed for this test suite. Make sure that a binary file is output.
-   */
-  flags: {
-    "--debug": [],
-
-    /** This is required. Do not change this. */
-    "--binaryFile": ["output.wasm"],
+export default {
+  entries: ["assembly/__tests__/**/*.spec.ts"],
+  include: ["assembly/__tests__/**/*.include.ts"],
+  disclude: [/node_modules/],
+  async instantiate(memory, createImports, instantiate, binary) {
+    let instance;
+    const myImports = {
+      env: { memory }
+    };
+    instance = instantiate(binary, createImports(myImports));
+    return instance;
   },
-  /**
-   * A set of regexp that will disclude source files from testing.
-   */
-  disclude: [],
-  /**
-   * Add your required AssemblyScript imports here.
-   */
-  imports: {},
-  /**
-   * Add a custom reporter here if you want one
-   */
-  // reporter: new CustomReporter(),
+  outputBinary: false,
 };
